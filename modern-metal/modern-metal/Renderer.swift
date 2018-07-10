f41e129a6d87f352f01b20d691f35f88199ffb49
Warren Moore <wm@warrenmoore.net>
2018-07-10 23:37:02 +0900

    Added drawing and animation code

--- modern-metal/modern-metal/Renderer.swift
+++ modern-metal/modern-metal/Renderer.swift
@@ -1,19 +1,32 @@
 
 import Foundation
 import MetalKit
+import simd
+
+struct Uniforms {
+    var modelViewMatrix: float4x4
+    var projectionMatrix: float4x4
+}
 
 class Renderer: NSObject, MTKViewDelegate {
     
     let device: MTLDevice
     let mtkView: MTKView
+    let commandQueue: MTLCommandQueue
+    var renderPipeline: MTLRenderPipelineState!
     var vertexDescriptor: MTLVertexDescriptor!
     var meshes: [MTKMesh] = []
+    var time: Float = 0
     
     init(view: MTKView, device: MTLDevice) {
         self.mtkView = view
         self.device = device
+        self.commandQueue = device.makeCommandQueue()!
+
         super.init()
+
         loadResources()
+        buildPipeline()
     }
     
     func loadResources() {
@@ -38,9 +51,72 @@ class Renderer: NSObject, MTKViewDelegate {
         }
     }
     
+    func buildPipeline() {
+        guard let library = device.makeDefaultLibrary() else {
+            fatalError("Could not load default library from main bundle")
+        }
+        
+        let vertexFunction = library.makeFunction(name: "vertex_main")
+        let fragmentFunction = library.makeFunction(name: "fragment_main")
+        
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        
+        pipelineDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat
+        
+        pipelineDescriptor.vertexDescriptor = vertexDescriptor
+        
+        do {
+            renderPipeline = try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch {
+            fatalError("Could not create render pipeline state object: \(error)")
+        }
+    }
+    
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
     }
     
     func draw(in view: MTKView) {
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+        
+        if let renderPassDescriptor = view.currentRenderPassDescriptor, let drawable = view.currentDrawable {
+            let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+            
+            time += 1 / Float(mtkView.preferredFramesPerSecond)
+            let angle = -time
+            let modelMatrix = float4x4(rotationAbout: float3(0, 1, 0), by: angle) *  float4x4(scaleBy: 2)
+
+            let viewMatrix = float4x4(translationBy: float3(0, 0, -2))
+            let modelViewMatrix = viewMatrix * modelMatrix
+            let aspectRatio = Float(view.drawableSize.width / view.drawableSize.height)
+            let projectionMatrix = float4x4(perspectiveProjectionFov: Float.pi / 3, aspectRatio: aspectRatio, nearZ: 0.1, farZ: 100)
+            
+            var uniforms = Uniforms(modelViewMatrix: modelViewMatrix, projectionMatrix: projectionMatrix)
+            
+            commandEncoder.setVertexBytes(&uniforms, length: MemoryLayout<Uniforms>.size, index: 1)
+            
+            commandEncoder.setRenderPipelineState(renderPipeline)
+            
+            for mesh in meshes {
+                let vertexBuffer = mesh.vertexBuffers.first!
+                commandEncoder.setVertexBuffer(vertexBuffer.buffer, offset: vertexBuffer.offset, index: 0)
+                
+                for submesh in mesh.submeshes {
+                    let indexBuffer = submesh.indexBuffer
+                    commandEncoder.drawIndexedPrimitives(type: submesh.primitiveType,
+                                                         indexCount: submesh.indexCount,
+                                                         indexType: submesh.indexType,
+                                                         indexBuffer: indexBuffer.buffer,
+                                                         indexBufferOffset: indexBuffer.offset)
+                }
+            }
+            
+            commandEncoder.endEncoding()
+            
+            commandBuffer.present(drawable)
+            
+            commandBuffer.commit()
+        }
     }
 }
---
+++ modern-metal/modern-metal/Shaders.metal
@@ -0,0 +1,36 @@
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexIn {
+    float3 position  [[attribute(0)]];
+    float3 normal    [[attribute(1)]];
+    float2 texCoords [[attribute(2)]];
+};
+
+struct VertexOut {
+    float4 position [[position]];
+    float4 eyeNormal;
+    float4 eyePosition;
+    float2 texCoords;
+};
+
+struct Uniforms {
+    float4x4 modelViewMatrix;
+    float4x4 projectionMatrix;
+};
+
+vertex VertexOut vertex_main(VertexIn vertexIn [[stage_in]],
+                             constant Uniforms &uniforms [[buffer(1)]])
+{
+    VertexOut vertexOut;
+    vertexOut.position = uniforms.projectionMatrix * uniforms.modelViewMatrix * float4(vertexIn.position, 1);
+    vertexOut.eyeNormal = uniforms.modelViewMatrix * float4(vertexIn.normal, 0);
+    vertexOut.eyePosition = uniforms.modelViewMatrix * float4(vertexIn.position, 1);
+    vertexOut.texCoords = vertexIn.texCoords;
+    return vertexOut;
+}
+
+fragment float4 fragment_main(VertexOut fragmentIn [[stage_in]]) {
+    return float4(1, 0, 0, 1);
+}
