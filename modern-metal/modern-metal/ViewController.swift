53c3428263485bb111697cfb5c0cf2975a97cd75
Warren Moore <wm@warrenmoore.net>
2018-07-10 22:30:20 +0900

    Added Renderer class and wired up the MTKView

--- modern-metal/modern-metal/ViewController.swift
+++ modern-metal/modern-metal/ViewController.swift
@@ -3,9 +3,26 @@ import UIKit
 import MetalKit
 
 class ViewController: UIViewController {
-
+    
+    var mtkView: MTKView!
+    var renderer: Renderer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mtkView = MTKView()
+        mtkView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mtkView)
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
+        
+        let device = MTLCreateSystemDefaultDevice()!
+        mtkView.device = device
+        
+        mtkView.colorPixelFormat = .bgra8Unorm
+        
+        renderer = Renderer(view: mtkView, device: device)
+        mtkView.delegate = renderer
     }
 }
 
